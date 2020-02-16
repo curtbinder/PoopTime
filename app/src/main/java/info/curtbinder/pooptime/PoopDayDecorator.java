9ee2d679f71d210190e83f06b398b7bc67a1909a
Curt Binder <curt.binder@gmail.com>
2020-02-16 14:22:47 -0600

    Simplified day decorating

--- app/src/main/java/info/curtbinder/pooptime/PoopDayDecorator.java
+++ app/src/main/java/info/curtbinder/pooptime/PoopDayDecorator.java
@@ -1,6 +1,7 @@
 package info.curtbinder.pooptime;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.RectShape;
@@ -10,13 +11,14 @@ import androidx.core.content.ContextCompat;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 
 public class PoopDayDecorator implements DayViewDecorator {
 
     private final Context ctx;
 //    private final int bgColor;
 
-    private int dayCount;
+//    private int dayCount;
 //    private TextDrawable poopDrawable;
 //    private LayerDrawable container;
     private Drawable bg;
@@ -25,7 +27,7 @@ public class PoopDayDecorator implements DayViewDecorator {
     public PoopDayDecorator(Context ctx) {
         this.ctx = ctx;
 //        bgColor = ContextCompat.getColor(ctx, R.color.primaryColor);
-        dayCount = 0;
+//        dayCount = 0;
 
         ShapeDrawable square = new ShapeDrawable(new RectShape());
         square.getPaint().setColor(ContextCompat.getColor(ctx, R.color.primaryColor));
@@ -40,19 +42,14 @@ public class PoopDayDecorator implements DayViewDecorator {
         // Look up the day in the database
         // If the DAY is in it, we decorate the day
         String date = DBCommands.getDefaultDayOnlyFormatString(day.getDay(), day.getMonth(), day.getYear());
-        dayCount = DBCommands.getPoopCountForDay(ctx, date);
-        boolean fRet = false;
+        return DBCommands.isDayLogged(ctx, date);
+//        dayCount = DBCommands.getPoopCountForDay(ctx, date);
+//        boolean fRet = false;
 //        Log.d("PoopDay", date + ": " + dayCount);
-        if ( dayCount > 0 ) {
-//            poopDrawable = new TextDrawable(ctx);
-//            poopDrawable.setText(Integer.toString(dayCount));
-//            container = new LayerDrawable(new Drawable[]{bg, poopDrawable});
-//            container.setLayerInset(1, inset, inset, 0, 0);
-            fRet = true;
-//        } else {
-//            container = new LayerDrawable(new Drawable[]{bg});
-        }
-        return fRet;
+//        if ( dayCount > 0 ) {
+//            fRet = true;
+//        }
+//        return fRet;
     }
 
     @Override
